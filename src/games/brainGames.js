a7f53f71f2e138c1945acfa9aff1a57d0adf98bd
Maxim Roganov <mroganov@dclab.local>
2021-07-21 14:38:50 +0300

    fix hexlet ci tests error

--- src/games/brainGames.js
+++ src/games/brainGames.js
@@ -8,30 +8,6 @@ import brainProgression from './brainProgression.js';
 const brainGames = () => {
   const name = getName();
   userWelcome(name);
-
-  console.log('What game would you like to play?');
-
-  const choosenGame = getGameName();
-
-  switch (choosenGame) {
-    case 'calc':
-      brainCalc(name, false);
-      break;
-    case 'even':
-      brainEven(name, false);
-      break;
-    case 'gcd':
-      brainGcd(name, false);
-      break;
-    case 'prime':
-      brainPrime(name, false);
-      break;
-    case 'progression':
-      brainProgression(name, false);
-      break;
-    default:
-      console.log('You dont choose a game');
-  }
 };
 
 export default brainGames;
